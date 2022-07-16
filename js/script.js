09291a8d8132c363fbd99c32342ba42e561f8288
joaoviss@gmail.com <joaoviss@gmail.com>
2022-07-16 19:31:20 -0300

    Tentando implantar a detecção de colisão

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+var lives = 3;
+var rings = 0;
 //* 
 const bgmGreenHill = document.querySelector('#bgm-green-hill');
 const sfxDeath = document.querySelector('#sfx-death');
@@ -13,6 +15,8 @@ const bgmSwitch = document.querySelector('#bgm-switch');
 const buttons = document.querySelector('#buttons');
 const jumpBtn = document.querySelector('#jump-btn');
 const slideBtn = document.querySelector('#slide-btn');
+const lifeCounter = document.querySelector('#life-counter');
+const ringCounter = document.querySelector('#ring-counter');
 // */
 const sonic = new Sonic();
 const enemies = [
@@ -31,16 +35,20 @@ const show = (element) => {
 
 const init = () => {
     bgmControll();
-    titleScreen.addEventListener('click', () => {
-        show(titleScreen);
-        introBox.style.animation = 'intro-box 3s ease both';
-        introBox.addEventListener('animationend', () => {
-            sonic.intro();
-            [hud, buttons].forEach(element => {
-                show(element);
-            });
-            bgmSwitch.click();
+    show(titleScreen);
+    introBox.style.animation = 'intro-box 3s ease both';
+    lifeCounter.textContent = sonic.lives;
+    ringCounter.textContent = rings;
+    introBox.addEventListener('animationend', () => {
+        sonic.intro();
+        [hud, buttons].forEach(element => {
+            show(element);
         });
+        // round();
+        bgmSwitch.click();
+        setInterval(() => {
+            round();
+        }, 2000);
     });
 }
 
@@ -53,25 +61,64 @@ const bgmControll = () => {
     });
 }
 
+//*
 const colision = (enemy) => {
-    let s = sonic.figure.getBoundingClientRect();
-    let e = enemy.getBoundingClientRect();
+    setInterval(() => {
+            let s = sonic.figure.getBoundingClientRect();
+            let e = enemy.figure.getBoundingClientRect();
+            if ((s.x + s.width > e.x) && (s.x < e.x + e.width)
+            && (s.y < e.y + e.height) && (s.y + s.height > e.y)) {
+                if (sonic.canKill) {
+                    enemy.canKill = false;
+                    enemy.death();
+                    rings += parseInt(enemy.figure.alt);
+                    ringCounter.textContent = rings;
+                } else {
+                    if (enemy.canKill) {
+                        sonic.death();
+                        enemy.canKill = false;
+                        lifeCounter.textContent = sonic.lives;
+                        enemy.figure.src = './img/explode.gif';
+                        enemy.figure.style.animationPlayState = 'paused';
+                        setTimeout(() => {
+                            enemy.figure.style.animationPlayState = 'running';
+                            try {
+                                // enemy.figure.parentNode.removeChild(enemy.figure);
+                            } catch(error) {
+                                console.log(error);
+                            }
+                        }, 300);
+                    }
+                }
+            }
+    }, 100);
 }
+//  */
 
+/*
 const round = () => {
     let n = Math.floor(Math.random() * enemies.length);
-    let enemy = enemies[n].figure;
+    let enemy = enemies[n];
+    stage.appendChild(enemy.figure);
+}
+
+/*/
+const round = () => {
+    let n = Math.floor(Math.random() * enemies.length);
+    let enemy = enemies[n];
     setTimeout(() => {
-        stage.appendChild(enemy);
+        stage.appendChild(enemy.figure);
         colision(enemy);
-        enemy.addEventListener('animationend', () => {
-            stage.removeChild(enemy)
+        enemy.figure.addEventListener('animationend', () => {
+            // enemy.figure.parentNode.removeChild(enemy.figure);
+                stage.removeChild(enemy.figure);
         });
     }, 1000);
 }
+//  */
 
 document.addEventListener('DOMContentLoaded', () => {
-    init();
+    titleScreen.addEventListener('click', init);
     jumpBtn.addEventListener('click', () => sonic.jump());
     slideBtn.addEventListener('click', () => sonic.slide());
 });
